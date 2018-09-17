1ba72a7a90fd57599b29a6b8a63cc6d2721f3548
luke zhang <sirsargras@gmail.com>
2018-09-17 00:57:17 -0500

    09_17_2018

--- elec478/hwk2/hwk2_2.py
+++ elec478/hwk2/hwk2_2.py
@@ -78,8 +78,11 @@ if iter_r==max_iter-1:
 mat.figure()
 mat.plot(range(0,iter_r+1),error_list)
 
-
+b=[4.5,6]
+b=np.reshape(b,(len(b),1))
 x=np.zeros((len(b),1))
+x_min=b
+
 
 #||x-b||_2^2
 #contant step size
@@ -87,12 +90,53 @@ max_iter=100
 error_list=[]
 iter_r=0
 for i in range(0,max_iter):
-    ak=1
+    ak=0.1
     x=x-ak*2*(x-b)
     #x=x-ak*np.multiply(np.sqrt(np.absolute(2*(x-b))),np.divide(2*(x-b),np.absolute(2*(x-b))))
     error_temp=norm(x_min-x)/norm(x_min)
     error_list.append(error_temp)
-  
+    if error_temp <=0.01:
+        iter_r=i
+        break
+    iter_r=i
+
+if iter_r==max_iter-1:
+    print ('failed to converge')
+mat.figure()
+mat.plot(range(0,iter_r+1),error_list)
+
+
+
+x=np.zeros((len(b),1))
+max_iter=100
+error_list=[]
+iter_r=0
+for i in range(0,max_iter):
+    ak=(1/6)**i
+    x=x-ak*2*(x-b)
+    #x=x-ak*np.multiply(np.sqrt(np.absolute(2*(x-b))),np.divide(2*(x-b),np.absolute(2*(x-b))))
+    error_temp=norm(x_min-x)/norm(x_min)
+    error_list.append(error_temp)
+    if error_temp <=0.01:
+        iter_r=i
+        break
+    iter_r=i
+
+if iter_r==max_iter-1:
+    print ('failed to converge')
+mat.figure()
+mat.plot(range(0,iter_r+1),error_list)
+
+x=np.zeros((len(b),1))
+max_iter=10000
+error_list=[]
+iter_r=0
+for i in range(0,max_iter):
+    ak=(1/4/(i+1))
+    x=x-ak*2*(x-b)
+    #x=x-ak*np.multiply(np.sqrt(np.absolute(2*(x-b))),np.divide(2*(x-b),np.absolute(2*(x-b))))
+    error_temp=norm(x_min-x)/norm(x_min)
+    error_list.append(error_temp)
     if error_temp <=0.01:
         iter_r=i
         break
