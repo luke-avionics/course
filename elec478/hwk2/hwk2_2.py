2a0c7041e55a58d017d2b840f7782b513d1601a6
luke zhang <sirsargras@gmail.com>
2018-09-17 21:56:37 -0500

    09-17-2018-21-56

--- elec478/hwk2/hwk2_2.py
+++ elec478/hwk2/hwk2_2.py
@@ -10,12 +10,12 @@ x_min=b
 
 #||x-b||_2
 #contant step size
-max_iter=100
+max_iter=4000
 error_list=[] 
 iter_r=0
 for i in range(0,max_iter):
     ak=1
-    x=x-ak*2*(x-b)/norm(x-b)
+    x=x-ak*(x-b)/norm(x-b)
     #x=x-ak*np.multiply(np.sqrt(np.absolute(2*(x-b))),np.divide(2*(x-b),np.absolute(2*(x-b))))
     error_temp=norm(x_min-x)/norm(x_min)
     error_list.append(error_temp)
@@ -26,21 +26,24 @@ for i in range(0,max_iter):
 
 if iter_r==max_iter-1:
     print ('failed to converge')
+else:
+    print(iter_r)
+
 mat.figure()
 mat.plot(range(0,iter_r+1),error_list) 
-
-
+mat.title('MSE vs. Iterations (ak=1)')
+mat.xlabel('Iterations')
+mat.ylabel('MSE')
 
 x=np.zeros((len(b),1))
 
 
 #(5/6)^k
-max_iter=100
 error_list=[]
 iter_r=0
 for i in range(0,max_iter):
     ak=(5/6)**i
-    x=x-ak*2*(x-b)/norm(x-b)
+    x=x-ak*(x-b)/norm(x-b)
     #x=x-ak*np.multiply(np.sqrt(np.absolute(2*(x-b))),np.divide(2*(x-b),np.absolute(2*(x-b))))
     error_temp=norm(x_min-x)/norm(x_min)
     error_list.append(error_temp)
@@ -50,21 +53,25 @@ for i in range(0,max_iter):
     iter_r=i
 if iter_r==max_iter-1:
     print ('failed to converge')
+else:
+    print(iter_r)
 
 mat.figure()
 #mat.hold(True)
 mat.plot(range(0,iter_r+1),error_list)
+mat.title('MSE vs. Iterations (ak=(5/6)^k)')
+mat.xlabel('Iterations')
+mat.ylabel('MSE')
 
 x=np.zeros((len(b),1))
 
 
 #1/(k+1)
-max_iter=100
 error_list=[]
 iter_r=0
 for i in range(0,max_iter):
     ak=1/(i+1)
-    x=x-ak*2*(x-b)/norm(x-b)
+    x=x-ak*(x-b)/norm(x-b)
     #x=x-ak*np.multiply(np.sqrt(np.absolute(2*(x-b))),np.divide(2*(x-b),np.absolute(2*(x-b))))
     error_temp=norm(x_min-x)/norm(x_min)
     error_list.append(error_temp)
@@ -75,8 +82,17 @@ for i in range(0,max_iter):
 
 if iter_r==max_iter-1:
     print ('failed to converge')
+else:
+    print(iter_r)
+
+
 mat.figure()
 mat.plot(range(0,iter_r+1),error_list)
+mat.title('MSE vs. Iterations (ak=1/(k+1))')
+mat.xlabel('Iterations')
+mat.ylabel('MSE')
+
+
 
 b=[4.5,6]
 b=np.reshape(b,(len(b),1))
@@ -86,7 +102,6 @@ x_min=b
 
 #||x-b||_2^2
 #contant step size
-max_iter=100
 error_list=[]
 iter_r=0
 for i in range(0,max_iter):
@@ -102,13 +117,18 @@ for i in range(0,max_iter):
 
 if iter_r==max_iter-1:
     print ('failed to converge')
+else:
+    print(iter_r)
+
 mat.figure()
 mat.plot(range(0,iter_r+1),error_list)
+mat.title('MSE vs. Iterations (ak=0.1)')
+mat.xlabel('Iterations')
+mat.ylabel('MSE')
 
 
 
 x=np.zeros((len(b),1))
-max_iter=100
 error_list=[]
 iter_r=0
 for i in range(0,max_iter):
@@ -124,11 +144,16 @@ for i in range(0,max_iter):
 
 if iter_r==max_iter-1:
     print ('failed to converge')
+else:
+    print(iter_r)
+
 mat.figure()
 mat.plot(range(0,iter_r+1),error_list)
+mat.title('MSE vs. Iterations (ak=(1/6)^k)')
+mat.xlabel('Iterations')
+mat.ylabel('MSE')
 
 x=np.zeros((len(b),1))
-max_iter=10000
 error_list=[]
 iter_r=0
 for i in range(0,max_iter):
@@ -144,8 +169,14 @@ for i in range(0,max_iter):
 
 if iter_r==max_iter-1:
     print ('failed to converge')
+else:
+    print(iter_r)
+
 mat.figure()
 mat.plot(range(0,iter_r+1),error_list)
+mat.title('MSE vs. Iterations (ak=1/4(i+1))')
+mat.xlabel('Iterations')
+mat.ylabel('MSE')
 
 
 mat.show()
--- elec478/hwk2/hwk2_3.py
+++ elec478/hwk2/hwk2_3.py
@@ -6,13 +6,9 @@ import scipy.io as sio
 
 #cross validation
 def cross_val(X,y,k,lambd):
-    #randomize = np.arange(len(X))
-    #np.random.shuffle(randomize)
-    #X = X[randomize]
-    #y = y[randomize]
     error=[]
     for i in range(0,k):
-        if k!=4:
+        if i!=k-1:
             X_test=X[i*int(len(X)/5):(i+1)*int(len(X)/5)]
             X_train=np.concatenate((X[:i*int(len(X)/5)],X[(i+1)*int(len(X)/5):]),axis=0)
             y_test=y[i*int(len(X)/5):(i+1)*int(len(X)/5)]
@@ -39,7 +35,7 @@ with open("hitters.x.csv", "r") as f:
     X_colnames = next(csv.reader(f))
 # load salaries
 y = np.loadtxt("hitters.y.csv", delimiter=",", skiprows=1)
-
+#calculate mean variance of the data
 X_mean=np.mean(X,axis=0)
 X_std=np.std(X,axis=0)
 y_mean=np.mean(y,axis=0)
@@ -51,29 +47,58 @@ X=np.concatenate((np.ones((len(X),1)),X),axis=1)
 y=np.reshape(y,(len(y),1))
 lambd=np.logspace(-3,7,100,base=10)
 theta_r=[]
+#prepare identity matrix to eliminate the penalization of biase term
 I=np.identity(np.shape(X)[1])
+theta_ind=np.empty((len(lambd),np.shape(X)[1]))
 I[0][0]=0 
+counter=0
+#calculate theta for each lambda
 for i in lambd:
+    theta_ind[counter,:]=np.reshape(np.absolute(inv(np.transpose(X)@X+i*I)@np.transpose(X)@y),(1,np.shape(X)[1]))
     theta_r.append(norm(inv(np.transpose(X)@X+i*I)@np.transpose(X)@y))
+    counter+=1
 theta_l=norm(inv(np.transpose(X)@X)@np.transpose(X)@y)*np.ones((np.shape(lambd)[0],1))
+#ploting ridge regression results
 mat.figure()
-mat.semilogx(lambd,theta_r)
+mat.loglog(lambd,theta_r)
+#plotting least square results
 mat.hold(True)
-mat.semilogx(lambd,theta_l)
+mat.loglog(lambd,theta_l)
+mat.title("Norm of theta vs. Lambda values")
+mat.legend(['Ridge regression results','Least square results'])
 mat.hold(False)
 
+#plotting absolute values of each feature against lambda
+#decide which features are important
+lines = ["-","--","-.",":"]
+mat.figure()
+mat.hold(True)
+for i in range(0,len(theta_ind[1,:])):
+    indx_style=int(i/5)
+    mat.semilogx(lambd,theta_ind[:,i],lines[indx_style])
+mat.legend([str(i) for i in range(0,len(theta_ind[1,:]))])
+mat.title("Abs of each feature vs. Lambda values")
+
+
+
+#shuffle the data for cross validation
 randomize = np.arange(len(X))
 np.random.shuffle(randomize)
 X = X[randomize]
 y = y[randomize]
 
 
+#do cross validation for eahc lambda values
 error=[]
 for i in lambd:
     error.append(cross_val(X,y,5,i))
+#plot the cross validation results
 mat.figure()
 mat.semilogx(lambd,error)
-
+mat.title('MSE vs. Lambda values (minimized when lambda='+str(round(lambd[np.argmin(error)],3))+')')
+best_lambd=lambd[np.argmin(error)]
+best_theta=inv(np.transpose(X)@X+best_lambd*I)@np.transpose(X)@y
+print (best_theta)
 
 
 mat.show()
